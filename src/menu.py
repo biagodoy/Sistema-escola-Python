0f9421d021b49e55a30f8fcc43db374c64537d86
biagodoy <beatrizgodoy381@gmail.com>
2024-04-19 18:04:52 -0300

    aula 3 criando menu

--- src/menu.py
+++ src/menu.py
@@ -2,3 +2,20 @@ def MostrarMenu():
     print("menu pricipal")
     print("1. cadastrar aluno")
     print("2. listar alunos")
+    # colocar terceira opcao cadastrar notas
+    
+    # Criar lógica que pegar valor do terminal
+    # e toma uma decisão atráves do valor inserido.
+
+    entradaUsuario = input()
+
+    nomeDoAluno = ""
+
+    if entradaUsuario == "1": 
+        print('Cadastrando Aluno..')
+        print("Insira o nome do aluno")
+        nomeDoAluno = input()
+        
+    if entradaUsuario == "2":
+        print('Listando Alunos...')
+        print(nomeDoAluno)
